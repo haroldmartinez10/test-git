59f200e0f7c64129d3d70e126abf644fcc0a401e
Harold Martinez <haroldmartinez_5@hotmail.com>
2023-02-14 08:43:03 -0500

    Add users settings

--- counter.js
+++ counter.js
@@ -1,9 +1,1 @@
-const numero1 = 5;
-
-const numero2 = 10;
-
-console.log(numero1 + numero2)
-
-const numberx2 = (number) => {
-return number * 2
-}
+console.log("Hello world")
---
+++ public/users.js
@@ -0,0 +1,1 @@
+const users = [{name:'Harold'}]
